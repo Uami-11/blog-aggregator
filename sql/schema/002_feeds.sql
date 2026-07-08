b4b495055a663e16098cb300a14153bf316ccee9
Uami-11 <nirwan.m.9618@gmail.com>
2026-07-08 10:51:28 +0545

    fix: make the types of string and id consistent by making them not null

--- sql/schema/002_feeds.sql
+++ sql/schema/002_feeds.sql
@@ -1,11 +1,11 @@
 -- +goose up
 CREATE TABLE feeds (
     id uuid PRIMARY KEY,
-    name varchar(255),
-    url varchar(255) UNIQUE,
+    name varchar(255) NOT NULL,
+    url varchar(255) NOT NULL UNIQUE,
     created_at timestamp,
     updated_at timestamp,
-    user_id uuid REFERENCES users (id) ON DELETE CASCADE
+    user_id uuid REFERENCES users (id) ON DELETE CASCADE NOT NULL
 );
 
 -- +goose down
