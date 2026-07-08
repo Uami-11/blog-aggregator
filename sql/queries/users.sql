f9168374db882c922b609ea530be40201e9d010a
Uami-11 <nirwan.m.9618@gmail.com>
2026-07-08 10:52:10 +0545

    add: make a create feeds query

--- sql/queries/users.sql
+++ sql/queries/users.sql
@@ -1,22 +1,23 @@
 -- name: CreateUser :one
-
 INSERT INTO users (id, created_at, updated_at, name)
-VALUES (
-    $1,
-    $2,
-    $3,
-    $4
-)
-RETURNING *;
+    VALUES ($1, $2, $3, $4)
+RETURNING
+    *;
 
 -- name: GetUser :one
-
-SELECT * FROM users WHERE name=$1;
+SELECT
+    *
+FROM
+    users
+WHERE
+    name = $1;
 
 -- name: DeleteUsers :exec
-
-TRUNCATE TABLE users;
+TRUNCATE TABLE users CASCADE;
 
 -- name: GetUsers :many
+SELECT
+    name
+FROM
+    users;
 
-SELECT name FROM users;
